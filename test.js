26056a7b7b252013ac17df8fc5ad864c1a44fb78
Nathan Vander Wilt <nate@calftrail.com>
2013-05-30 16:06:20 -0700

    verifying that spinning up a server in test suite works fine (and still exits when tests are done)

--- test.js
+++ test.js
@@ -1,13 +1,35 @@
 var request = require('supertest');
 
+var HUB_URL = process.env.HUB_URL,
+    CB_URL = process.env.CB_URL,
+    PORT = process.env.PORT || 8000;
+
 // assumes PUBLISHER_URL="http://ipcalf.com"
 // i.e. not actually testing PubSubHubbub yet
 
-describe('Hello World.', function() {
-  it('respond with json', function (done) {
-    request(process.env.PUBLISHER_URL)
-      .get("?format=json")
-      .expect('Content-Type', /json/)
+describe('PubSubHubbub', function() {
+  it('hub is alive', function (done) {
+    request(HUB_URL).get("/")
+      .expect('Content-Type', /html/)
       .expect(200, done);
   });
+  it('callback server is alive', function (done) {
+    request(CB_URL).get("/")
+      .expect('Content-Type', /plain/)
+      .expect(200, done);
+  });
+  
 });
+
+
+
+// callback server (only active as long as Mocha runs)
+var connect = require('connect');
+
+var app = connect()
+    .use(connect.logger('dev'))
+    .use(function(req, res) {
+        res.writeHead(200, {'Content-Type': "text/plain"});
+        res.end('hello world\n');
+    })
+.listen(PORT);
